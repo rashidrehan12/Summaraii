d38461475b27da42c78c39f8ce3df7f963665045
Md Rashid <118841846+rashidrehan12@users.noreply.github.com>
2025-10-25 18:36:08 +0530

    Remove unused import for PromptTemplate

--- cs.py
+++ cs.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from langchain.prompts import PromptTemplate
 from langchain_groq import ChatGroq
 from langchain.chains.summarize import load_summarize_chain
 from langchain_community.document_loaders import PyPDFLoader
